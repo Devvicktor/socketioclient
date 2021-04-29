cd03a2b0406435427f8d589d3da975874d55c43a
Devvicktor <kosgeivick90@gmail.com>
2021-04-30 01:12:47 +0300

    client

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io('https://socketioworking.herokuapp.com/ ');
 
 const form = document.getElementById('form');
 const input = document.getElementById('input');

68f860b47d2c270ce85bc6aa89eb2fbba34d5c6d
Devvicktor <kosgeivick90@gmail.com>
2021-05-01 11:18:08 +0300

    skovi

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = io ('http://localhost:3000');
+const socket = io ('https://socketioworking.herokuapp.com/');
 const userList = document.getElementById('users');
 const roomName = document.getElementById('room-name');
 const form = document.getElementById ('form');
